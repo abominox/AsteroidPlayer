4725121824fbb062f59682f6de823e43f8c4d31b
RaxEmRemy <pwnagepal@gmail.com>
2016-05-26 21:24:26 -0400

    Fixed the file path for background that was broken after the last commit.

---
+++ asteroidgame/src/AsteroidFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+class AsteroidFrame extends JFrame
+{
+    public AsteroidFrame()
+    {
+    setTitle("Asteroids");
+    setSize(640,480);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+
+    setLayout(new BorderLayout());
+    setContentPane(new JLabel(new ImageIcon("/Users/christophermarquart/NetBeansProjects/AsteroidPlayer/asteroidgame/Media/star.jpg")));
+    setLayout(new FlowLayout());
+    setSize(640, 480);
+    }
+}
